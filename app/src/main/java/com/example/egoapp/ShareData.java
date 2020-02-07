650648e01665c40573dff5bed2eeea185fec4637
Mark <trungnguyen10051998@gmail.com>
2020-02-06 22:46:19 -0500

    First screen with sample data

--- app/src/main/java/com/example/egoapp/ShareData.java
+++ app/src/main/java/com/example/egoapp/ShareData.java
@@ -5,6 +5,19 @@ public class ShareData {
     public static String something = "";
     public static String something2 = "";
 
+    public static String mTitle[] = {"Toronto to Ottawa",
+            "Toronto to Waterloo",
+            "Toronto to Cambridge",
+            "Toronto to Quebec",
+            "Toronto to Vancouver"};
+
+    public static String mDescription[] = {"$140", "$50", "$100", "$200", "$500"};
+
+    public static int images[] = {R.drawable.ottawa,
+            R.drawable.waterloo,
+            R.drawable.cambridge,
+            R.drawable.quebec,
+            R.drawable.vancouver};
     // Make trip global variables
     public static String tripSelectedDate = "";
     public static String tripSelectedTime = "";
