f558b46039d8cf9729e0a856409eea545a10ad52
tnguyen5130 <trungnguyen10051998@gmail.com>
2020-03-12 19:57:40 -0400

    Revert "Merge branch 'Request-For-Make-Trip' into Make-Trip"
    This reverts commit b5a5758314dc1169b56d306ade9cdf572b24c349, reversing
    changes made to 532e2bc9daa215f94bf1cc843aa6ed22ca42c356.

--- app/src/main/java/com/example/egoapp/MainActivity.java
+++ app/src/main/java/com/example/egoapp/MainActivity.java
@@ -1,17 +1,14 @@
 //* FILE			: MainActivity.java
-//* PROJECT			: SENG2040-20W-Mobile Application Development - Assignment #2
-//* PROGRAMMER		: Nhung Luong, Hyunchul Choi, Trung Nguyen, Abdullar
-//* FIRST VERSION	: Mar 14, 2020
+//* PROJECT			: SENG2040-20W-Mobile Application Development - Assignment #1
+//* PROGRAMMER		: Nhung Luong, Younchul Choi, Trung Nguyen, Abdullar
+//* FIRST VERSION	: Feb 8, 2018
 //* DESCRIPTION		: The file defines the first screen of the app asking for getting trip info
 
 
 package com.example.egoapp;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,16 +22,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.example.egoapp.DBHandler.CityDB;
-import com.example.egoapp.Object.Cities;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -42,12 +30,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button searchBtn, makeNewTripBtn, viewTripBtn, aboutAppBtn;
     private DrawerLayout drawer;
 
-    private String TAG = MakeTrip.class.getSimpleName();
-    private ProgressDialog pDialog;
-    private static String url = "http://10.0.2.2:3000/cities/";
-    ArrayList<HashMap<String, String>> citiesList;
-
-    CityDB callerCity;
 
     /*
      * Function: onCreate
@@ -60,13 +42,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_main);
 
-        //
-        callerCity = new CityDB(this);
-
-        citiesList = new ArrayList<>();
-
-        // assign data from json-serer with cities
-        new GetContacts().execute();
 
         drawer = findViewById(R.id.drawer_layout);
 
@@ -162,123 +137,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-    }
-
-    private class GetContacts extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // Showing progress dialog
-            pDialog = new ProgressDialog(MainActivity.this);
-            pDialog.setMessage("Please wait...");
-            pDialog.setCancelable(false);
-            pDialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            HttpHandler sh = new HttpHandler();
-
-            // Making a request to url and getting response
-            String jsonStr = sh.makeServiceCall(url);
-
-            Log.e(TAG, "Response from url: " + jsonStr);
-
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-
-                    // Getting JSON Array node
-                    JSONArray cities = jsonObj.getJSONArray("cities");
-
-                    // looping through All Contacts
-                    Log.e(TAG, "Size: " + cities.length());
-                    for (int i = 0; i < cities.length(); i++) {
-                        JSONObject c = cities.getJSONObject(i);
-
-                        String id = c.getString("id");
-                        String startCity = c.getString("startCity");
-                        String endCity = c.getString("endCity");
-
-                        // tmp hash map for single contact
-                        HashMap<String, String> contact = new HashMap<>();
-
-                        contact.put("id", id);
-                        contact.put("startCity", startCity);
-                        contact.put("endCity", endCity);
-
-                        // adding contact to contact list
-                        citiesList.add(contact);
-                    }
-                } catch (final JSONException e) {
-                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "Json parsing error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    });
-                }
-            } else {
-                Log.e(TAG, "Couldn't get json from server.");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                "Couldn't get json from server. Check LogCat for possible errors!",
-                                Toast.LENGTH_LONG)
-                                .show();
-                    }
-                });
-
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            // Dismiss the progress dialog
-            if (pDialog.isShowing())
-                pDialog.dismiss();
-
-            /**
-             * Updating parsed JSON data into ListView
-             * */
-
-            List<String> listStartCity = new ArrayList<String>();
-            Log.e(TAG, "element size: " + citiesList.size());
-
-            for (int i = 0; i < citiesList.size(); i++) {
-                HashMap<String, String> map = citiesList.get(i);
-                Cities findCities = new Cities();
-
-                ArrayList<Cities> allCities = callerCity.getCities();
-                Log.e(TAG, "### Cities Size ### : " + allCities.size());
-
-                // check cities for existing cities
-                findCities = callerCity.findCity(map.get("startCity"));
-                if(findCities == null) {
-                    // new City ==> cityID assign
-                    Cities newCities = new Cities();
-                    Log.e(TAG, "### New City ###");
-                    // get cityID from table
-                    long rows = callerCity.getCityIDCount();
-                    newCities.setCityID(rows + 1);
-                    Log.e(TAG, "The cityID: " + newCities.getCityID());
-                    // get city Name from json-server
-                    newCities.setCityName(map.get("startCity"));
-
-                    // insert new city
-                    long insertCityId = callerCity.insertCity(newCities);
-                    Log.e(TAG, "New Cities: " + insertCityId);
-                }
-            }
-        }
     }
 }
