449e69ff27c5896b6bac128ca8e541eaea41c120
tnguyen5130 <trungnguyen10051998@gmail.com>
2020-04-15 15:28:32 -0400

    Need to fix UI for call button

--- app/src/main/java/com/example/egoapp/PhoneCall.java
+++ app/src/main/java/com/example/egoapp/PhoneCall.java
@@ -6,16 +6,22 @@
 package com.example.egoapp;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -23,12 +29,24 @@ import androidx.core.content.ContextCompat;
 
 public class PhoneCall extends AppCompatActivity {
 
+    TextView contactList;
+    EditText inputPhoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_call);
 
         Button call = (Button) findViewById( R.id.fab );
+        contactList = (TextView) findViewById(R.id.textView_phoneList);
+        inputPhoneNumber = (EditText) findViewById(R.id.editText_phoneNumber);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, Constant.MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            fetchContacts();
+        }
 
         call.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +54,7 @@ public class PhoneCall extends AppCompatActivity {
                 //----------------------when the call button is pressed-----------------------------
                 final int REQUEST_PHONE_CALL = 1;
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:1234567890"));
+                callIntent.setData(Uri.parse("tel:"+ inputPhoneNumber.getText()));
                 //-----------------checks for permission before placing the call--------------------
                 if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                     if(ContextCompat.checkSelfPermission(PhoneCall.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
@@ -48,6 +66,41 @@ public class PhoneCall extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void fetchContacts() {
+        String phoneNumber = null;
+        String email = null;
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+        StringBuffer output = new StringBuffer();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+        // Loop for every contact in the phone
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
+                if (hasPhoneNumber > 0) {
+                    output.append("\n First Name:" + name);
+                    // Query and loop for every phone number of the contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        output.append("\n Phone number:" + phoneNumber);
+                    }
+                    phoneCursor.close();
+                }
+                output.append("\n");
+            }
+            contactList.setText(output);
+        }
     }
 
     @Override
@@ -85,5 +138,25 @@ public class PhoneCall extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+   /* @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchContacts();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.d("My App", "permission denied");
+                }
+                return;
+            }
+        }
+    }*/
+
+    private class Constant {
+        public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
     }
 }
